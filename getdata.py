69e0271fc13b884b74100f8a8fe27d35fa07de01
RahulVaishanav <43729186+RahulVaishanav@users.noreply.github.com>
2018-10-01 10:14:48 -0700

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -1,5 +1,5 @@
 import psycopg2
-conn=psycopg2.connect(host="35.202.149.46" , user="postgres", database="postgres", password="Rahul")
+conn=psycopg2.connect(host="" , user="", database="", password="")
 cur=conn.cursor()
 cur.execute("select * from wrsn03 order by datetime desc limit 10")
 a= cur.fetchall()
